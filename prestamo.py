cdd245e57c780fe4f9da33dcefbb6887c509a116
Adrián Bedoya Patino <adrian.bedoya@iesdonana.org>
2021-02-22 19:05:49 +0100

    Añadido el metodo timedelta del modulo datetime al archivo prestamo.py

--- prestamo.py
+++ prestamo.py
@@ -1,13 +1,13 @@
 from lector import Lector
 from libro import Libro
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class Prestamo():
 
     def __init__(self, lector, libro, fecha_prestamo):
         self.__lector = lector
         self.__libro = libro
-        self.__fecha_prestamo = datetime(fecha_prestamo)
+        self.__fecha_prestamo = fecha_prestamo
         self.__fecha_devolucion = None
 
     def lector(self):
@@ -20,6 +20,6 @@ class Prestamo():
         return self.__fecha_prestamo
 
     def devuelve_libro(self, fecha_devolucion):
-        self.__fecha_devolucion = datetime(fecha_devolucion)
+        self.__fecha_devolucion = fecha_devolucion
         if self.__fecha_prestamo + timedelta(15) == self.__fecha_devolucion:
             self.__lector.set_moroso()
