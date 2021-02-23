3730fbbf73eb598a4cf6c54236ea10bbd37baeb7
Adrián Bedoya Patino <adrian.bedoya@iesdonana.org>
2021-02-23 16:50:17 +0100

    Solucion final

--- prestamo.py
+++ prestamo.py
@@ -4,11 +4,16 @@ from datetime import datetime, timedelta
 
 class Prestamo():
 
+    __libro_prestamos = {}
+
     def __init__(self, lector, libro, fecha_prestamo):
+        if libro.codigo() in Prestamo.__libro_prestamos:
+            raise NameError("Libro ya alquilado")
         self.__lector = lector
         self.__libro = libro
         self.__fecha_prestamo = fecha_prestamo
         self.__fecha_devolucion = None
+        Prestamo.__libro_prestamos[self.__libro.codigo()] = self.__libro.titulo()
 
     def lector(self):
         return self.__lector
@@ -21,5 +26,6 @@ class Prestamo():
 
     def devuelve_libro(self, fecha_devolucion):
         self.__fecha_devolucion = fecha_devolucion
-        if self.__fecha_prestamo + timedelta(15) > self.__fecha_devolucion:
+        if self.__fecha_prestamo + timedelta(15) < self.__fecha_devolucion:
             self.__lector.set_moroso()
+        del Prestamo.__libro_prestamos[self.__libro.codigo()]
--- principal.py
+++ principal.py
@@ -1,14 +1,30 @@
 from prestamo import Prestamo
 from libro import Libro
 from lector import Lector
-from datetime import datetime
+from datetime import date, datetime
 
+#Crear datos
 lector1 = Lector(0, "Ana", "Garcia")
 lector2 = Lector(1, "Roberto", "Sanchez")
 libro1 = Libro(0, "El camino", "Miguel Delibes", "Misco")
-libro2 =Libro(1, "")
-libro3 =
+libro2 = Libro(1, "Cien años de soledad", "Gabriel Garcia Marquez", "Misco")
+libro3 = Libro(2, "Rayuela", "Julio Cortazar", "Misco")
 prestamo1 = Prestamo(lector1, libro1, datetime(2021, 2, 22))
+prestamo2 = Prestamo(lector1, libro2, datetime(2021, 2, 23))
+prestamo3 = Prestamo(lector2, libro3, datetime(2021, 2, 23))
+
+#Devolver un libro prestado con menos de 15 dias
+print(f'El lector {lector2.nombre()} {lector2.apellidos()} es moroso: {lector2.moroso()}')
+prestamo3.devuelve_libro(datetime(2021, 2, 24))
+print(f'El lector {lector2.nombre()} {lector2.apellidos()} es moroso: {lector2.moroso()}')
+
+#Prestar un libro ya prestado
+#prestamo4 = Prestamo(lector2, libro1, datetime(2021, 2, 23))
+
+#Devolver un libro prestrado con mas de 15 dias
 print(f'El lector {lector1.nombre()} {lector1.apellidos()} es moroso: {lector1.moroso()}')
 prestamo1.devuelve_libro(datetime(2021, 4, 22))
 print(f'El lector {lector1.nombre()} {lector1.apellidos()} es moroso: {lector1.moroso()}')
+
+#Prestar un libro devuelto
+prestamo5 = Prestamo(lector2, libro1, datetime(2021, 2, 23))
